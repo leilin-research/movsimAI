c871a708719f98d34067966a8be6bb1c1309b2f4
josephkamel <josephekamel@gmail.com>
2017-10-25 14:49:34 +0200

    increase save freq

--- src/org/movsim/simulator/SimulationRunnable.java
+++ src/org/movsim/simulator/SimulationRunnable.java
@@ -93,7 +93,7 @@ public class SimulationRunnable extends SimulationRun implements Runnable {
 	private int sleepTime_ms;
 	private int sleepTimeUsed;
 	private static final double DEFAULT_TIMESTEP_S = 0.25; // default timestep, seconds
-	private static final int DEFAULT_SLEEP_TIME_MS = 20; // default sleep time, milliseconds
+	private static final int DEFAULT_SLEEP_TIME_MS = 10; // default sleep time, milliseconds
 	private boolean pausedWhenRunning;
 	private long lastUpdateTime_ms;
 	private double actualTimewarp = 0;
